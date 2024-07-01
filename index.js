4cc704b343d9c2c9a6b375a382d64fc7fd32b70c
eddah73 <eddahsjr@gmail.com>
2024-07-01 21:03:43 +0300

    find ()method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+
+  function superbowlWin(array) {
+    for (let element of array) {
+      if(element.result === "W"){
+        return element.year;
+      }
+      
+    }
+  }
+  record.find(superbowlWin);
+  superbowlWin(record);
+// const superbowlWin = array.find(item =>{
+//     return item.result === 'w'
+// })
+
+//   superbowlWin();
