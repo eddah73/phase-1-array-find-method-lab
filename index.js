0cd3c8052df3eb52031a85105c04def68742ac84
eddah73 <eddahsjr@gmail.com>
2024-07-02 15:37:15 +0300

    find method

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 
-  function superbowlWin(array) {
-    for (let element of array) {
-      if(element.result === "W"){
-        return element.year;
-      }
+  // function superbowlWin(array) {
+  //   for (let element of array) {
+  //     if(element.result === "W"){
+  //       return element.year;
+  //     }
       
+  //   }
+  // }
+  // record.find(superbowlWin);
+  // superbowlWin(record);
+  const superbowlWin = (array) => {
+    const firstWin = array.find((element) => element.result === "W");
+      if (firstWin){
+        return firstWin.year;
+      }else {
+        return undefined
+      }
     }
-  }
-  record.find(superbowlWin);
-  superbowlWin(record);
-// const superbowlWin = array.find(item =>{
-//     return item.result === 'w'
-// })
 
-//   superbowlWin();
+  // superbowlWin();
+  // const superbowlWin = (array) => {
+  //   const firstWin = array.find((element) => element.result === "W");
+  //   return firstWin.year
+  // }
